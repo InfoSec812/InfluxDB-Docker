756dbf60686c96132862f79ff6b0bb04d77d100f
Deven Phillips <deven.phillips@gmail.com>
2015-02-11 15:31:29 -0500

    Updated run.sh script to allow pre-creation of DB

--- docker/run.sh
+++ docker/run.sh
@@ -6,8 +6,9 @@ CONFIG_FILE="/config/config.toml"
 #Dynamically change the value of 'max-open-shards' to what 'ulimit -n' returns
 sed -i "s/^max-open-shards.*/max-open-shards = $(ulimit -n)/" ${CONFIG_FILE}
 
-if ! [ -n ${API_URL} ]; then
-    API_URL="http://${HOSTNAME}:8083"
+if [ "${API_URL}X" == "X" ]; then
+    echo "=> Setting API URL to default"
+    export API_URL="http://127.0.0.1:8086"
 fi
 
 #Configure InfluxDB Cluster
@@ -63,6 +64,9 @@ if [ -n "${SSL_SUPPORT}" ]; then
     sed -i -r -e 's/^# ssl-/ssl-/g' ${CONFIG_FILE}
 fi
 
+mkdir -p /data/admin
+echo "=> Starting InfluxDB ..."
+/usr/bin/influxdb -config=${CONFIG_FILE} &
 
 #Pre create database on the initiation of the container
 if [ -n "${PRE_CREATE_DB}" ]; then
@@ -71,7 +75,6 @@ if [ -n "${PRE_CREATE_DB}" ]; then
         echo "=> Database had been created before, skipping ..."
     else
         echo "=> Starting InfluxDB ..."
-        exec /usr/bin/influxdb -config=${CONFIG_FILE} &
         PASS=${INFLUXDB_INIT_PWD:-root}
         arr=$(echo ${PRE_CREATE_DB} | tr ";" "\n")
 
@@ -80,7 +83,8 @@ if [ -n "${PRE_CREATE_DB}" ]; then
         while [[ RET -ne 0 ]]; do
             echo "=> Waiting for confirmation of InfluxDB service startup ..."
             sleep 3 
-            curl -k ${API_URL}/ping 2> /dev/null
+            echo "curl -k ${API_URL}/ping"
+            curl -k ${API_URL}/ping
             RET=$?
         done
         echo ""
@@ -88,20 +92,15 @@ if [ -n "${PRE_CREATE_DB}" ]; then
         for x in $arr
         do
             echo "=> Creating database: ${x}"
-            curl -s -k -X POST -d "{\"name\":\"${x}\"}" $(echo ${API_URL}'/db?u=root&p='${PASS})
+            echo "curl -v -s -k -X POST -d \"{\"name\":\"${x}\"}\" \"${API_URL}/db?u=root&p=${PASS}\""
+            curl -v -s -k -X POST -d "{\"name\":\"${x}\"}" "${API_URL}/db?u=root&p=${PASS}"
         done
         echo ""
 
         touch "/data/.pre_db_created"
-        fg
-        exit 0
     fi
 else
     echo "=> No database need to be pre-created"
 fi
 
-mkdir -p /data/admin
-
-echo "=> Starting InfluxDB ..."
-
-/usr/bin/influxdb -config=${CONFIG_FILE}
+fg
